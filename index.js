7b11660eee0d4762fea4b0a2d7ac5d8ddd33502d
gabygarro <ggarroab@gmail.com>
2016-12-26 19:53:54 -0600

    Reporte a consola

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
 var TelegramBot = require('node-telegram-bot-api'),
-    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-    telegram = new TelegramBot("323827964:AAFeRVwFLAkeOzokjksBfrg4ERT7pkdE5hE", { polling: true });
+telegram = new TelegramBot("323827964:AAFeRVwFLAkeOzokjksBfrg4ERT7pkdE5hE", { polling: true });
 
-telegram.on("text", (message) => {
-  telegram.sendMessage(message.chat.id, "Hola mijo");
+telegram.on('message', function (message) {
+	// Registrar conversación a consola
+	var dt = new Date();
+	console.log(dt.toUTCString());
+	entrante = "\t" + message.chat.username + ": " + message.text;
+	console.log(entrante);
+	respuesta = "Hola mija/o";
+	console.log("\tYo: " + respuesta);
+	//Enviar respuesta
+	telegram.sendMessage(message.chat.id, respuesta);
 });
